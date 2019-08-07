50062fdbf45f8491d48b94f3013ac4e5bace6fc7
Eric Berglund <ericsamuelberglund@gmail.com>
2019-08-07 14:39:42 -0600

    Javascript basic info exercise

---
+++ javascriptIntro/basicinfo.js
@@ -0,0 +1,7 @@
+var name = prompt("What is your name");
+var lastname = prompt("What is your last name");
+var age = prompt("What is your age");
+alert("Your name is " + name + " " + lastname);
+alert("Your age is " + age);
+console.log("Your name is " + name + " " + lastname);
+console.log("Your age is " + age);
