f620f4ec066469417abc29a38e24a58fdcae6bea
Eric Berglund <ericsamuelberglund@gmail.com>
2019-08-22 18:04:06 +0000

    Add package.json and a few more routes to example app

--- firstExpressApp/app.js
+++ firstExpressApp/app.js
@@ -11,6 +11,14 @@ app.get("/bye", function(req, res){
 	res.send("Goodbye");
 })
 
+app.get("/r/:subredditName", function(req, res){
+	res.send("Welcome to subreddit " + req.params.subredditName);
+});
+
+app.get("*", function(req, res){
+	res.send("Catchall");
+});
+
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 })
