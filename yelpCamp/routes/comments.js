6a6b6f239ca7503b87d25f9e2584d76d71fd424d
Eric Berglund <ericsamuelberglund@gmail.com>
2019-08-29 19:34:26 +0000

    Add comment edit, update, destroy, and auth

--- yelpCamp/routes/comments.js
+++ yelpCamp/routes/comments.js
@@ -41,12 +41,69 @@ router.post("/", isLoggedIn, function(req, res){
 	});
 });
 
+//EDIT Route
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+	Comment.findById(req.params.comment_id, function(err, comment){
+		if(err){
+			res.redirect("back");	
+		}
+		else {
+			res.render("comments/edit", {campground_id: req.params.id, comment: comment});			
+		}
+	});
+});
+
+//Update Route
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, comment){
+		if(err){
+			res.redirect("back");
+		}
+		else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
+//Delete route
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err, comment){
+		if(err){
+			res.redirect("back");
+		}
+		else {
+			res.redirect("/campgrounds/" + req.params.id);
+		}
+	});
+});
+
 // Middleware
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, comment){
+			if(err){
+				res.redirect("back");
+			}
+			else {
+				if(comment.author.id.equals(req.user._id)){
+					next();
+				}
+				else {
+					res.redirect("back");
+				}
+			}
+		});
+	}
+	else {
+		res.redirect("back");
+	}	
 }
 
 module.exports = router;
